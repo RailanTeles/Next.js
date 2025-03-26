57acbfc8ba7b68a8a5b052f3cba3e748bed851c5
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-03-26 16:01:33 -0300

    Testando

--- app/Home/layout.tsx
+++ app/Home/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full bg-blue-700 min-h-screen flex flex-col relative justify-center items-center">
+    <div className="w-full bg-blue-700 min-h-screen flex flex-col">
       {children}
     </div>
   );
